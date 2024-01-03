c453f04a689a5d7922f6862a4206fba8d30579cb
nanacao <nhutmcao@gmail.com>
2024-01-03 19:56:53 +0200

    Set up cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const morgan = require('morgan');
 
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use(cors());
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(
@@ -83,7 +85,7 @@ app.post('api/persons', (req, res) => {
   }
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
